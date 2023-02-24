cc9416f7f7e95f0093c0aa0972eb34dec0a6da94
SantoshGeo21 <121722215+SantoshGeo21@users.noreply.github.com>
2023-02-24 23:09:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def index():
 
             
             
-            client = pymongo.MongoClient("mongodb+srv://santu12:santu123@cluster0.bbs81qi.mongodb.net/?retryWrites=true&w=majority")
+            client = pymongo.MongoClient("mongodb+srv://santu12:<password>@cluster0.bbs81qi.mongodb.net/?retryWrites=true&w=majority")
             db =client['scrapper_eng_pwskills']
             coll_pw_eng = db['scraper_pwskills_eng']
             coll_pw_eng.insert_many(reviews)
